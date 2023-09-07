12a7185c1f363fa5c9a64d89f8688cdd9ca3259d
Will <wmaples423@gmail.com>
2023-09-07 10:40:27 -0400

    update bg removal script to hit whole directory

--- DND/remove_bg_from_image.py
+++ DND/remove_bg_from_image.py
@@ -1,8 +1,24 @@
+import os
 from rembg import remove
 from PIL import Image
-import easygui as eg
-input_path = eg.fileopenbox(title='Select image file')
-output_path = eg.filesavebox(title='Save file to..')
-input = Image.open(input_path)
-output = remove(input)
-output.save(output_path)
+import easygui
+
+# display a file selection dialog to choose the directory containing the JPEG files
+directory = easygui.diropenbox(title="Select directory containing JPEG files")
+
+# loop through all files in the directory
+for filename in os.listdir(directory):
+    # check if the file is a JPEG file
+    if filename.endswith(".jpg") or filename.endswith(".jpeg"):
+        # create input and output paths
+        input_path = os.path.join(directory, filename)
+        output_path = os.path.join(directory, os.path.splitext(filename)[0] + ".png")
+        
+        # open the input image
+        input_image = Image.open(input_path)
+        
+        # remove the background from the input image
+        output_image = remove(input_image)
+        
+        # save the output image as a PNG file with the original name
+        output_image.save(output_path)
